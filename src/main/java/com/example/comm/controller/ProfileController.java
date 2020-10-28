380edff4ea62dd481fdb55b21d7c41742841efaf
fox1997 <1214241313@qq.com>
2020-10-28 17:38:17 +0800

    拦截器

--- src/main/java/com/example/comm/controller/ProfileController.java
+++ src/main/java/com/example/comm/controller/ProfileController.java
@@ -32,24 +32,7 @@ public class ProfileController {
             HttpServletRequest request,
             @RequestParam(name = "page",defaultValue = "1")Integer page,
             @RequestParam(name = "size",defaultValue = "3")Integer size){
-        User user=null;
-        Cookie[] cookies=request.getCookies();
-        if(cookies !=null && cookies.length!=0)
-        {
-            for (Cookie cookie : cookies)
-            {
-                if(cookie.getName().equals("token"))
-                {
-                    String token = cookie.getValue();
-                    user=userMapper.findByToken(token);
-                    if(user != null)
-                    {
-                        request.getSession().setAttribute("user",user);
-                    }
-                    break;
-                }
-            }
-        }
+        User user=(User)request.getSession().getAttribute("user");
         if(user==null){
             return "redirect:/";
         }
@@ -59,7 +42,6 @@ public class ProfileController {
         }else if("replies".equals(action)){
             model.addAttribute("section","replies");
             model.addAttribute("sectionName","最新回复");
-
         }
         PaginationDto paginationDto=questionService.list(user.getId(),page,size);
         model.addAttribute("pagination",paginationDto);
--- src/main/java/com/example/comm/controller/PublishController.java
+++ src/main/java/com/example/comm/controller/PublishController.java
@@ -1,7 +1,6 @@
 package com.example.comm.controller;
 
 import com.example.comm.mapper.QuestionMapper;
-import com.example.comm.mapper.UserMapper;
 import com.example.comm.model.Question;
 import com.example.comm.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
+
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
-    @Autowired
+    @Autowired(required = false)
     private QuestionMapper questionMapper;
-    @Autowired
-    private UserMapper userMapper;
+
 
     @GetMapping("/publish")
     public String publish()
@@ -54,28 +52,7 @@ public class PublishController {
             model.addAttribute("error","标签不能为空");
             return "publish";
         }
-
-        User user=null;
-
-        Cookie [] cookies = request.getCookies();
-        if(cookies!=null&&cookies.length!=0)
-        {
-            for(Cookie cookie : cookies)
-            {
-                if(cookie.getName().equals("token"))
-                {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if(user != null)
-                    {
-                        request.getSession().setAttribute("user",user);
-                    }
-                    break;
-                }
-            }
-
-        }
-
+        User user=(User)request.getSession().getAttribute("user");
         if(user == null)
         {
             model.addAttribute("error","用户未登录");
